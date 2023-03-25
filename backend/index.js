bb5843ab3736746d05211fcd0393b07f6cd687fe
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2023-03-25 14:58:04 +0530

    Added schema

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ const connectToMongo = require("./db");
 const express = require("express");
 const app = express();
 const port = 5000;
-app.get("/", (req, res) => {
-  res.send("Hello Bro!");
-});
+
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Bhai is listening on port ${port}`);
 });
 connectToMongo();
